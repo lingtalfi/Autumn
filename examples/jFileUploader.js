96362ccb197cd1b700ff7629c9286b0c05defe58
lingtalfi <lingtalfi@gmail.com>
2020-04-01 11:21:41 +0200

    update

---
+++ examples/jFileUploader.js
@@ -0,0 +1,78 @@
+const Autumn = require("autumn-wizard");
+
+
+var useWatch = true;
+
+
+
+const baseDir = "./libs/universe/Ling/JFileUploader";
+const w = new Autumn();
+w.watch([`${baseDir}/src/**`], () => {
+
+
+    const useSourceMap = true; // set this to false in production
+
+    //----------------------------------------
+    // CSS
+    //----------------------------------------
+    var cssFiles = [
+        `${baseDir}/src/scss/**/*.scss`,
+    ];
+    w.forEach(cssFiles, srcPath => {
+        var dstPath = w.replace('/src/', '/dist/', srcPath);
+        dstPath = w.replace('scss', 'css', dstPath);
+        dstPath = w.replace('.css', '.min.css', dstPath);
+        w.minify(srcPath, dstPath, {
+            sourceMap: useSourceMap,
+        });
+    });
+
+
+    //----------------------------------------
+    // BUNDLE THE JS MODULES
+    //----------------------------------------
+    var srcPath = `${baseDir}/src/js/main.js`;
+    var dstPath = `${baseDir}/dist/js/main.min.js`;
+    w.bundle(srcPath, dstPath, {
+        debug: useSourceMap,
+    });
+
+    var themeFiles = [
+        `${baseDir}/src/js/views/**/*.js`,
+    ];
+    w.forEach(themeFiles, srcPath => {
+        dstPath = w.replace("/src/", '/dist/', srcPath);
+        dstPath = w.replace(".js", '.min.js', dstPath);
+        w.bundle(srcPath, dstPath, {
+            debug: useSourceMap,
+        });
+    });
+
+
+
+    //----------------------------------------
+    // MOVE THE LANG FILES (and uglify them)
+    //----------------------------------------
+    var otherFiles = [
+        `${baseDir}/src/js/lang/**/*.js`,
+    ];
+    w.forEach(otherFiles, srcPath => {
+        dstPath = w.replace("/src/", '/dist/', srcPath);
+        dstPath = w.replace(".js", '.min.js', dstPath);
+        w.uglify(srcPath, dstPath, {
+            sourceMap: useSourceMap
+        });
+    });
+
+
+    //----------------------------------------
+    // CREATE THE HANDLEBARS TEMPLATES
+    //----------------------------------------
+    var tplPaths = [
+        `${baseDir}/src/js/templates/**/*.hbs`,
+    ];
+    dstPath = `${baseDir}/dist/js/templates/bundled.min.js`;
+    w.precompile(tplPaths, dstPath);
+
+
+}, useWatch);
